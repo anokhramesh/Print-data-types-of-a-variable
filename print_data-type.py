65c66558e2e35a1f48d35468089c57a503c86984
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-07-07 20:06:45 +0400

    Add files via upload

---
+++ print_data-type.py
@@ -0,0 +1,37 @@
+# This is an Example Program to Display the data type of variables in Pythom programming.
+
+name = "ramesh kumar tharayil sadanandan"# assigned a string  to variable-'name' 
+print (type(name))# display the data type of 'name' variable.
+
+op = ('+')# assigned a character to variable-'op' 
+print (type(op))# display the data type of 'op' variable.
+
+age = 70 # assigned a integer value of 70 to variable-'age' 
+print (type(age))# display the data type of 'age' variable.
+
+pi = 3.14 # assigned a float value to variable-'pi' 
+print (type(pi)) # display the data type of 'pi' variable.
+
+yes = True # assigned a boolian  to variable-'yes' 
+print(type(yes) )# display the data type of 'yes' variable.
+
+d = {'mango':1,'orange':8} # assigned a dictionary to variable-'d' 
+print(type(d)) #display the data type of 'd' variable.
+
+l = [8,3.14,'name',True] # assigned a list to variable-'l' 
+print(type(l)) #display the data type of 'l' variable.
+
+s = set() #assigned an empty set to variable-'s' 
+print(type(s)) #display the data type of 's' variable.
+
+s1 = {4.0,'string',True}# assigned a set of elements to variable-'s1' 
+print(type(s1))# display the data type of 's1' variable.
+
+t1 = (4.0,"string",True)# assigned a tuple data type to variable-'t1' 
+print(type(t1))# display the data type of 't1' variable.
+
+b = bytes([11,10,17,15])# assigned a bytes type data to variable 'b'
+print (type(b))# display the type of variable 'b'
+
+c = 3+2.7j# assigned a complex type data to variable name 'c'
+print (type(c))# display the data type of variable 'c'
